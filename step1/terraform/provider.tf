07c728c79ec665f560215c8d4437deb3cb06d5ba
mehdilaruelle <mehdi.laruelle@d2si.io>
2020-05-02 17:26:48 +0200

    update Vault Terraform provider and Vault

--- step1/terraform/provider.tf
+++ step1/terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "vault" {
-  version = "~> 2.6.0"
+  version = "~> 2.10.0"
 
   address = "http://vault:8200"
 }
--- step4/terraform/provider.tf
+++ step4/terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "vault" {
-  version = "~> 2.0.0"
+  version = "~> 2.10.0"
 
   address = "http://vault:8200"
 }
