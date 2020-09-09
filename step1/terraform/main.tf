c02d0810353eb5053133834a982c498e1061fa48
mehdilaruelle <mehdi.laruelle@d2si.io>
2020-09-10 00:50:29 +0200

    Remove warning from terraform v0.12

--- step1/terraform/main.tf
+++ step1/terraform/main.tf
@@ -6,29 +6,29 @@ data "template_file" "web_policies" {
   template = "${file("${var.policy_path}")}"
 
   vars = {
-    entity_name = "${var.entity_name}"
+    entity_name = var.entity_name
   }
 }
 
 resource "vault_policy" "web_policies" {
-  name = "${var.entity_name}"
+  name = var.entity_name
 
-  policy = "${data.template_file.web_policies.rendered}"
+  policy = data.template_file.web_policies.rendered
 }
 
 resource "vault_approle_auth_backend_role" "project_role" {
-  backend            = "${vault_auth_backend.approle.path}"
+  backend            = vault_auth_backend.approle.path
   role_name          = "role-${var.entity_name}"
-  secret_id_num_uses = "${var.token_num_uses}"
-  secret_id_ttl      = "${var.secret_id_ttl}"
-  token_num_uses     = "${var.token_num_uses}"
-  token_ttl          = "${var.token_ttl}"
-  token_max_ttl      = "${var.token_max_ttl}"
-  token_policies     = ["default", "${var.entity_name}"]
+  secret_id_num_uses = var.token_num_uses
+  secret_id_ttl      = var.secret_id_ttl
+  token_num_uses     = var.token_num_uses
+  token_ttl          = var.token_ttl
+  token_max_ttl      = var.token_max_ttl
+  token_policies     = ["default", var.entity_name]
 }
 
 # The pipeline should generate this one
 resource "vault_approle_auth_backend_role_secret_id" "id" {
-  backend   = "${vault_auth_backend.approle.path}"
-  role_name = "${vault_approle_auth_backend_role.project_role.role_name}"
+  backend   = vault_auth_backend.approle.path
+  role_name = vault_approle_auth_backend_role.project_role.role_name
 }
--- step3/terraform/main.tf
+++ step3/terraform/main.tf
@@ -16,37 +16,37 @@ data "template_file" "web_policies" {
   template = "${file("${var.policy_path}")}"
 
   vars = {
-    entity_name = "${var.entity_name}"
+    entity_name = var.entity_name
   }
 }
 
 resource "vault_policy" "web_policies" {
-  name = "${var.entity_name}"
+  name = var.entity_name
 
-  policy = "${data.template_file.web_policies.rendered}"
+  policy = data.template_file.web_policies.rendered
 }
 
 resource "vault_approle_auth_backend_role" "project_role" {
-  backend            = "${vault_auth_backend.approle.path}"
+  backend            = vault_auth_backend.approle.path
   role_name          = "role-${var.entity_name}"
-  secret_id_num_uses = "${var.token_num_uses}"
-  secret_id_ttl      = "${var.secret_id_ttl}"
-  token_num_uses     = "${var.token_num_uses}"
-  token_ttl          = "${var.token_ttl}"
-  token_max_ttl      = "${var.token_max_ttl}"
-  token_policies     = ["default", "${var.entity_name}"]
+  secret_id_num_uses = var.token_num_uses
+  secret_id_ttl      = var.secret_id_ttl
+  token_num_uses     = var.token_num_uses
+  token_ttl          = var.token_ttl
+  token_max_ttl      = var.token_max_ttl
+  token_policies     = ["default", var.entity_name]
 }
 
 # The pipeline should generate this one
 resource "vault_approle_auth_backend_role_secret_id" "id" {
-  backend   = "${vault_auth_backend.approle.path}"
-  role_name = "${vault_approle_auth_backend_role.project_role.role_name}"
+  backend   = vault_auth_backend.approle.path
+  role_name = vault_approle_auth_backend_role.project_role.role_name
 }
 
 resource "vault_database_secret_backend_connection" "mysql" {
-  backend       = "${vault_mount.db.path}"
+  backend       = vault_mount.db.path
   name          = "mysql"
-  allowed_roles = ["${var.entity_name}"]
+  allowed_roles = [var.entity_name]
 
   mysql {
     connection_url = "${var.db_user}:${var.db_password}@tcp(mysql:3306)/"
@@ -54,11 +54,11 @@ resource "vault_database_secret_backend_connection" "mysql" {
 }
 
 resource "vault_database_secret_backend_role" "role" {
-  backend             = "${vault_mount.db.path}"
-  name                = "${var.entity_name}"
-  db_name             = "${vault_database_secret_backend_connection.mysql.name}"
+  backend             = vault_mount.db.path
+  name                = var.entity_name
+  db_name             = vault_database_secret_backend_connection.mysql.name
   creation_statements = ["CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT ALL PRIVILEGES ON *.* TO '{{name}}'@'%';"]
-  default_ttl         = "${var.db_secret_ttl}"
+  default_ttl         = var.db_secret_ttl
 }
 
 resource "vault_generic_secret" "transit_key" {
